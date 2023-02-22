ff073e547cb575782f1fad97f81ccf874d171fc5
Schrotti17 <thom.st@protonmail.com>
2023-02-22 13:49:04 -0600

    Initial Commit

---
+++ FileSharing-iOS/FileSharing_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileSharing_iOSApp.swift
+//  FileSharing-iOS
+//
+//  Created by mm507d1 on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct FileSharing_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
