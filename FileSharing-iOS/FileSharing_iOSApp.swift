69e69a956f6e6da6e1c907979de41f49bf399d06
Schrotti17 <thom.st@protonmail.com>
2023-02-22 20:55:04 +0100

    Folders

--- FileSharing-iOS/FileSharing_iOSApp.swift
+++ FileSharing-iOS/FileSharing_iOSApp.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import RealmSwift
+import FirebaseStorage
+import FirebaseCore
+
+let app = App(id: "filesharingmobile-dqwye")
+let storage = Storage.storage(url: "gs://filesharingjf.appspot.com")
+// Create a storage reference from our storage service
+let storageRef = storage.reference()
+
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+    FirebaseApp.configure()
+
+    return true
+
+  }
+
+}
 
 @main
-struct FileSharing_iOSApp: App {
+struct FileSharing_iOS_App: SwiftUI.App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(app: app)
         }
     }
 }
